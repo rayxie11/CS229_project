1aa28f069738c2bfb7096dc31d69a155d4a6eb44
Jiaxuan Su <suj98@stanford.edu>
2022-05-05 17:25:47 -0700

    update

--- utils.py
+++ utils.py
@@ -27,7 +27,8 @@ This function converts all labels in dataset to onehot labels
 Args:
     file_location: A string for the file location of dataset labels
 Returns:
-    A dictionary with file names as keys and onehot labels (np.array) as values
+    file: A list of file names (n,)
+    label: A 2d numpy array of file labels (n,11)
 '''
 def onehot_labels(file_location):
     cur_dir = os.path.dirname(__file__)
@@ -35,12 +36,15 @@ def onehot_labels(file_location):
     f = open(parent_dir+'/dataset/'+file_location)
     data = json.load(f)
     f.close()
-    
-    for key in data:
-        label = data[key]
-        data[key] = convert_onehot(label)
 
-    return data
+    label = []
+    file = []
+    for key in data:
+        file.append(key)
+        label.append(convert_onehot(data[key]))
+    label = np.vstack(label)
+
+    return file, label
 
 '''
 This function matches labels with actual actions
@@ -64,11 +68,11 @@ def matching(file_location):
     return match_dict
 
 '''
-This function extracts the first frame of video in 2D matrix
+This function extracts the first frame of video
 Args:
     video_location: A string for the location of video to be captured
 Returns:
-    A 2d matrix of extracted first frame image
+    A 1d greyscale vector reshaped from 2d matrix (22528,)
 Exception:
     First frame capture fails
 '''
@@ -86,40 +90,34 @@ def extract_first_frame(video_location):
 '''
 This function extracts the first frame of all video data and saves as an npy file
 Args:
-    None
+    file: A list of file names returned from onehot_labels
 Returns:
-    A dictionary with file names (string) as keys and 2d image matrix as values
+    A 2d numpy array of greyscale vectors (n,22528)
 '''
-def frame_to_data():
+def frame_to_data(file):
     cur_dir = os.path.dirname(__file__)
     parent_dir = os.path.split(cur_dir)[0]
-    dir = parent_dir+'/dataset/examples/'
+    dir = parent_dir + '/dataset/examples/'
     vid_end = '.mp4'
-    img_dict = dict()
-    i = 0
-    for vid_file in os.scandir(dir):
-        if i > 2000:
-            break
-        if vid_file.name.endswith(vid_end):
-            img_name = vid_file.name[:len(vid_file.name)-4]
-            img_dict[img_name] = extract_first_frame(dir+vid_file.name)
-            i += 1
-    
-    np.save('img_dict.npy',img_dict)
-    
-    return img_dict
 
+    img = []
+    for name in file:
+        img.append(extract_first_frame(dir+name+vid_end))
+
+    np.save('img.npy', np.vstack(img))
+
+    return np.vstack(img)
 
 def main():
-    '''
-    data = onehot_labels('annotation_dict.json')
-    print(data)
-    data1 = matching("labels_dict.json")
-    print(data1)
-    '''
-    img = frame_to_data()
-    print(img)
-    
+
+    file, label = onehot_labels('annotation_dict.json')
+    #print(file)
+    # data1 = matching("labels_dict.json")
+    # print(data1)
+
+    img = frame_to_data(file)
+    print(img.shape)
+
 
 if __name__ == '__main__':
     main()
