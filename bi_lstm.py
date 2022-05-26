4b59ce52cabc2d36892c9ae2a06042f00ecb959e
wangtingkang <508425683@qq.com>
2022-05-25 23:09:57 -0700

    bi-directional LSTM model trained

---
+++ bi_lstm.py
@@ -0,0 +1,37 @@
+import numpy as np
+from keras import layers, Model
+
+class rnn_model():
+    def __init__(self, num_neurons, num_densor):
+        joint_input = layers.Input(shape=(16, 36))
+        X = layers.BatchNormalization()(joint_input)
+        X = layers.Bidirectional(layers.LSTM(num_neurons))(X)
+        X = layers.Dropout(0.3)(X)
+        X = layers.Dense(num_densor, activation='relu')(X)
+        X = layers.Dropout(0.2)(X)
+        X = layers.Dense(num_densor, activation='relu')(X)
+        X = layers.Dropout(0.2)(X)
+        X = layers.Dense(11)(X)
+        Y = layers.Activation('softmax')(X)
+        self.model = Model(inputs=joint_input, outputs=Y)
+
+    def train(self, X, Y, epochs, batch_size):
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model.fit(X, Y, epochs=epochs, batch_size=batch_size)
+
+    def test(self, X, Y):
+        self.model.evaluate(X, Y)
+
+if __name__ == '__main__':
+    X = np.load('motion.npy')
+    Y = np.load('label.npy')
+    X_train = X[:25000, :, :]
+    Y_train = Y[:25000, :]
+    X_dev = X[25000:30000, :, :]
+    Y_dev = Y[25000:30000, :]
+    X_test = X[30000:35000, :, :]
+    Y_test = Y[30000:35000, :]
+    model = rnn_model(50, 30)
+    model.train(X_train, Y_train, 100, 128)
+    model.test(X_dev, Y_dev)
+    model.test(X_test, Y_test)
